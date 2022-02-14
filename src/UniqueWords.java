20b2f2a6aa43b1e9c750250a2f8aac389ce078b4
Gliu240 <99309045+Gliu240@users.noreply.github.com>
2022-02-14 12:51:24 -0500

    Updated UniqueWords.java
    This is the completed UniqueWords java file.

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,8 +1,8 @@
+package Lab2;
 import java.util.ArrayList;
 
-public class UniqueWords
-{
-   /**
+public class UniqueWords {
+	/**
 		counts the number of unique strings in a list
 		@param list ArrayList of strings to be examined
 		@return number of unique strings in the list
@@ -12,26 +12,34 @@ public class UniqueWords
 	  int count = 0;
 	  
       for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
+      {	
+    	  boolean unique= true;
+    	  for (int j = 0; j < i; j++)
+    	  {
+    		  if(list.get(i) == list.get(j)) {
+    			unique = false;
+    			break;
+    		  }
+    	  }
+    	  if(unique) {
+    		  count++;
+    	  }
       }
+      
 	  return count;
    }
 
-   public static void main(String[] args)
-   {
-      ArrayList <String> words = new ArrayList<>();
-      words.add("apple");
-	  words.add("orange");
-	  words.add("blackboard");
-	  words.add("apple");
-	  words.add("orange");
-	  words.add("sun");
-	  words.add("moon");
-   
-	  int unique = countUnique(words);
-      System.out.println(words + " has " + unique + " unique words");
-   }
+	public static void main(String[] args) {
+		ArrayList<String> words = new ArrayList<>();
+		words.add("apple");
+		words.add("orange");
+		words.add("blackboard");
+		words.add("apple");
+		words.add("orange");
+		words.add("sun");
+		words.add("moon");
+
+		int unique = countUnique(words);
+		System.out.println(words + " has " + unique + " unique words");
+	}
 }
